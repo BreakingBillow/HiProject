3afe380821698944acee880de205dda036fdb864
hwu <hwu@Breakingbillow.CHINASZ.midtronics.com>
2017-03-02 17:30:44 +0800

    Test Mybatis

--- cn.hiproject.pm.server/src/main/java/cn/hiproject/pm/server/MybatisTest.java
+++ cn.hiproject.pm.server/src/main/java/cn/hiproject/pm/server/MybatisTest.java
@@ -1,25 +1,45 @@
 package cn.hiproject.pm.server;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 
 import cn.hiproject.pm.server.mybatis.common.MybatisUtils;
 import cn.hiproject.pm.server.mybatis.dao.AccountMapper;
+import cn.hiproject.pm.server.mybatis.model.Account;
+import cn.hiproject.pm.server.mybatis.model.AccountExample;
 
 public class MybatisTest {
 
+	static SqlSessionFactory sqlSessionFactory = null;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		SqlSession session = MybatisUtils.getSqlSession();
-
+		sqlSessionFactory = MybatisUtils.getSqlSessionFactory();
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		
+		List accountList = new ArrayList<Account>();
+		
 		try {
 			//Visitor visitor = (Visitor) session.selectOne("david.mybatis.demo.IVisitorOperation.basicQuery", id);
 			
+			AccountMapper accountDao = sqlSession.getMapper(AccountMapper.class);
+			
+			AccountExample example = new AccountExample();
+			accountList = accountDao.selectByExample(example);
 		
-			MybatisUtils.closeSession(session);
+			System.out.println(accountList);
+			
+			sqlSession.close();
 
 		} catch (Exception e) {
 			// TODO: handle exception
+			
+		} finally {
+			sqlSession.close();
 		}
 
 	}
